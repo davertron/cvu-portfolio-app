d036223844dedf9c86ba91bd19b6aa4189909fb2
Isaac Krementsov <isaackrementsov@gmail.com>
2021-05-06 16:30:39 -0400

    Added Tailwind.css

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Nav from '../lib/client/components/Nav';
+
+export default function Home() {
+    return (
+        <>
+            <Nav/>
+            <div className="container mx-auto px-4 py-4">
+                Portfolio App
+            </div>
+        </>
+    )
+}
