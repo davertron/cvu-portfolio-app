2914249c5a64163458a1d84c7c4efe6287c1ee90
Dave Davis <dabukun@gmail.com>
2021-09-01 07:39:14 -0400

    Don't reprompt for google drive permissions every time

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -47,7 +47,7 @@ export default NextAuth({
             clientId: process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID,
             clientSecret: process.env.OAUTH_CLIENT_SECRET,
             authorizationUrl:
-                'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+                'https://accounts.google.com/o/oauth2/v2/auth?prompt=select_account&access_type=offline&response_type=code',
             scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
         }),
     ],
